e967862425184f91547658fd12bf518505752b38
Stan Bakalov <stan.bakalov@podium.com>
2021-02-09 16:20:51 -0700

    errors work properly on the backend

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,13 +5,13 @@ import ItemsTable from '../components/ItemsTable';
 import { SubmitButton } from '../components/Buttons';
 
 const Home = () => {
-  // const [rows, setRows] = useState([
-  //   { name: "hello", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
-  //   { name: "hello", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
-  //   { name: "hello", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
-  //   { name: "hello", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" }
-  // ]);
-  const [rows, setRows] = useState([]);
+  const [rows, setRows] = useState([
+    { name: "hello", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
+    { name: "world", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
+    { name: "my", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" },
+    { name: "name", category: "asdf", price: "$12.12", source: "walmart", quantity: "4", comments: "Hello my name is", breakeven: "$15.14", asin: "1236345" }
+  ]);
+  // const [rows, setRows] = useState([]);
   const [inputs, setInputs] = useState({
     name: '',
     category: '',
@@ -22,44 +22,39 @@ const Home = () => {
     breakeven: '',
     asin: ''
   });
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({
+    name: '',
+    category: '',
+    price: '',
+    source: '',
+    quantity: ''
+  });
 
   const handleInput = (e, key) => {
     setInputs({ ...inputs, [key]: e.target.value });
-    console.log("INPUTS", inputs)
   };
 
   const handleValidation = inputs => {
-    const inputErrors = {};
     const numberRegex = /^[0-9\b]+$/;
+    const inputErrors = {};
 
-    if (inputs.name === '') {
+    if (!inputs.name) {
       inputErrors.name = 'Please enter an item name.';
     }
-    if (inputs.category === '') {
+    if (!inputs.category) {
       inputErrors.category = 'Please enter a category.';
     }
-    if (inputs.price === '') {
+    if (!inputs.price) {
       inputErrors.price = 'Please enter an item price.';
     }
-    if (inputs.source === '') {
+    if (!inputs.source) {
       inputErrors.source = 'Please enter a source.';
     }
-    if (inputs.quantity === '') {
+    if (!inputs.quantity) {
       inputErrors.quantity = 'Please enter a quantity.';
+    } else if (!numberRegex.test(inputs.quantity)) {
+      inputErrors.quantity = 'Invalid quantity.';
     }
-
-    // if (!inputs.xEmbedId) {
-    //   inputErrors.xEmbedId = 'Please enter a Domo Embed ID.';
-    // } else if (!domoEmbedIdRegex.test(inputs.xEmbedId)) {
-    //   inputErrors.xEmbedId = 'Invalid Domo Embed ID.';
-    // }
-
-    // if (!inputs.order) {
-    //   inputErrors.order = 'Please enter an order.';
-    // } else if (!numberRegex.test(inputs.order)) {
-    //   inputErrors.order = 'Invalid order number.';
-    // }
     return inputErrors;
   };
 
@@ -68,10 +63,17 @@ const Home = () => {
     const inputErrors = handleValidation(inputs);
     const hasErrors = Object.keys(inputErrors).length !== 0;
 
+    console.log("INPUT ERRORS", inputErrors)
     if (hasErrors) {
       setErrors(inputErrors);
     } else {
-      setErrors({});
+      setErrors({
+        name: '',
+        category: '',
+        price: '',
+        source: '',
+        quantity: ''
+      });
       addRow(inputs);
       setInputs({
         name: '',
